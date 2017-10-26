e27d31feff94f5756e30470a890262591ebb836c
Vyhyrynskyi Oleksii <vigirinskiy_kharkov@mail.ru>
2017-10-26 16:29:02 +0300

    master branch was merged with newServlet branch

--- src/main/java/com/teaminternational/web/BaseHttpServlet.java
+++ src/main/java/com/teaminternational/web/BaseHttpServlet.java
@@ -1,9 +1,14 @@
 package com.teaminternational.web;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public abstract class BaseHttpServlet extends HttpServlet {
-
+	
     protected <T> T getBean(String name) {
         return (T) getServletContext().getAttribute(name);
     }
