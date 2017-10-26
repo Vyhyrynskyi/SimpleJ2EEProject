9c68a2e0464fdac7127855a7e4ddd3c014aa0b51
Vyhyrynskyi Oleksii <vigirinskiy_kharkov@mail.ru>
2017-10-26 15:59:23 +0300

    Initial commit

--- src/main/java/com/teaminternational/web/BaseHttpServlet.java
+++ src/main/java/com/teaminternational/web/BaseHttpServlet.java
@@ -1,9 +1,22 @@
 package com.teaminternational.web;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.teaminternational.HelloWorldGenerator;
+import com.teaminternational.HelloWorldGeneratorImpl;
 
 public abstract class BaseHttpServlet extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HelloWorldGeneratorImpl helloWorld = getBean("HelloWorldGenerator");
+		helloWorld.sayHello();
+	}
     protected <T> T getBean(String name) {
         return (T) getServletContext().getAttribute(name);
     }
