b82f2fb9b8dc52892c9eaf57b696377c84eb2cb7
Vyhyrynskyi Oleksii <vigirinskiy_kharkov@mail.ru>
2017-10-26 16:16:29 +0300

    New Servlet was created

--- src/main/java/com/teaminternational/web/BaseHttpServlet.java
+++ src/main/java/com/teaminternational/web/BaseHttpServlet.java
@@ -7,16 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.teaminternational.HelloWorldGenerator;
-import com.teaminternational.HelloWorldGeneratorImpl;
-
 public abstract class BaseHttpServlet extends HttpServlet {
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		HelloWorldGeneratorImpl helloWorld = getBean("HelloWorldGenerator");
-		helloWorld.sayHello();
-	}
+	
     protected <T> T getBean(String name) {
         return (T) getServletContext().getAttribute(name);
     }
---
+++ src/main/java/com/teaminternational/web/NewServlet.java
@@ -0,0 +1,21 @@
+package com.teaminternational.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.teaminternational.HelloWorldGenerator;
+import com.teaminternational.HelloWorldGeneratorImpl;
+import com.teaminternational.web.BaseHttpServlet;;
+
+public class NewServlet extends BaseHttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HelloWorldGeneratorImpl helloWorld = getBean(HelloWorldGenerator.COMPONENT_NAME);
+		helloWorld.sayHello();
+	}
+}
